43507bd4c1c2d5475a1a00ebcbdad80b29a20092
Alan Escárcega <alan.escarcegadominguez@cesunbc.edu.mx>
2026-05-25 10:42:22 -0700

    Primer avance de desarrollo de apps en Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton }
+
+  from '@ionic/angular/standalone';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton],
 })
 export class HomePage {
-  constructor() {}
+
+  tasks: Task[] = [
+    {
+      id: 1, titulo: 'Configuracion de Ionic',
+      descripcion: 'Instalar Node.js, AngularCLI',
+      finalizado: true,
+      prioridad: 'Alta'
+
+    },
+
+    {
+      id: 2,
+      titulo: 'Crear app tasklist',
+      descripcion: 'Desarrollar la interfaz de la pagina principal',
+      finalizado: false,
+      prioridad: 'Alta'
+    }
+
+  ];
+
+
+  constructor() {
+    console.log(this.tasks);
+  }
+
+  saludar() {
+    console.log('Este es mi primer boton en Ionic');
+    console.log(this.tasks);
+  }
 }
