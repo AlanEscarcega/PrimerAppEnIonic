4aa6caf6ef133ec7960b6f07b60b9ba3c75be8ee
Alan Escárcega <alan.escarcegadominguez@cesunbc.edu.mx>
2026-05-28 21:10:29 -0700

    4 validaciones del 28 de mayo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {
   IonHeader, IonToolbar, IonTitle, IonContent,
   IonCard, IonCardContent, IonCardHeader,
-  IonCardTitle, IonCardSubtitle, IonButton
+  IonCardTitle, IonCardSubtitle, IonButton,
+  IonItem, IonLabel, IonInput, IonList,
 } from '@ionic/angular/standalone';
 
 import { Task } from '../models/task.model';
@@ -14,36 +16,55 @@ import { Task } from '../models/task.model';
   styleUrls: ['home.page.scss'],
   imports: [
     CommonModule,
+    FormsModule,
     IonHeader, IonToolbar, IonTitle, IonContent,
     IonCard, IonCardContent, IonCardHeader,
-    IonCardTitle, IonCardSubtitle, IonButton
+    IonCardTitle, IonCardSubtitle, IonButton,
+    IonItem, IonLabel, IonInput, IonList, IonItem
   ],
 })
 export class HomePage {
 
+  newTaskStr: string = '';
+
   tasks: Task[] = [
-    {
-      id: 1,
-      title: 'Configuracion de Ionic',
-      description: 'Instalar Node.js, Angular CLI',
-      completed: true,
-      priority: 'high'
-    },
-    {
-      id: 2,
-      title: 'Crear app tasklist',
-      description: 'Desarrollar la interfaz de la pagina principal',
-      completed: false,
-      priority: 'high'
-    }
+
   ];
 
   constructor() {
     console.log(this.tasks);
   }
 
-  saludar() {
-    console.log('Este es mi primer boton en Ionic');
+  addTask() {
+    console.log(this.newTaskStr);
+
+    const title = this.newTaskStr.trim();
+
+    if (!title) {
+      return;
+    }
+
+    const exists = this.tasks.some(
+      task => task.title.trim().toLowerCase() === title.toLowerCase()
+    );
+
+    if (exists) {
+      alert('Tarea repetida');
+      return;
+    }
+
+    const newTask: Task = {
+      id: Date.now(),
+      title: title,
+      description: '',
+      completed: false,
+      priority: 'medium'
+    };
+
+    this.tasks.push(newTask);
+    this.newTaskStr = ''; // Limpia el input
+
     console.log(this.tasks);
   }
+
 }
